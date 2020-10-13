9b367d9b9640c98a66db9626ab8d818e77b3634b
chensongsong <chensongsong@geetest.com>
2020-10-13 14:57:28 +0800

    [ 新增详情页面 ]

--- app/src/main/java/com/song/xposed/DetailsActivity.java
+++ app/src/main/java/com/song/xposed/DetailsActivity.java
@@ -15,10 +15,10 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,5 +27,4 @@ public class DetailsActivity extends AppCompatActivity {
             }
         });
     }
-
 }
